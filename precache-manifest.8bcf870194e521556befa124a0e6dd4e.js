9acb12aba29fac5c7097f863fa99dcf5c9e93c65
lennard978 <shparavalo78nenad@gmail.com>
2020-06-21 22:07:44 +0200

    Updates

---
+++ precache-manifest.8bcf870194e521556befa124a0e6dd4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a176fd80825bda770faf4b1b5faf24",
+    "url": "/reactQuiz/index.html"
+  },
+  {
+    "revision": "a68399f0be04325b047d",
+    "url": "/reactQuiz/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9aec7a3c9435534bc121",
+    "url": "/reactQuiz/static/css/main.42bbf43f.chunk.css"
+  },
+  {
+    "revision": "a68399f0be04325b047d",
+    "url": "/reactQuiz/static/js/2.6382e8d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactQuiz/static/js/2.6382e8d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aec7a3c9435534bc121",
+    "url": "/reactQuiz/static/js/main.c1fed018.chunk.js"
+  },
+  {
+    "revision": "9764250976fbd9a11bae",
+    "url": "/reactQuiz/static/js/runtime-main.b0315df3.js"
+  }
+]);
